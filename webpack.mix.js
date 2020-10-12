ed3a535187b317d52546d85c47b7fe361f37bcd6
Osimba <osiqua@gmail.com>
2020-10-12 15:05:44 -0700

    Set environment to production

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,3 +12,10 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js');
+
+/* mix.webpackConfig({
+    if(process.env.NODE_ENV === 'production') {
+
+    }
+    mode: 'production'
+}); */
